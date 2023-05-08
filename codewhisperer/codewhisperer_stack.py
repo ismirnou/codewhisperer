6363b651f26577430c7179108e90ba00bdc11ebf
Igor_Smirnou <Igor.Smirnou@coxautoinc.com>
2023-05-08 12:01:16 +0300

    Add codewhisperer infra resources

--- codewhisperer/codewhisperer_stack.py
+++ codewhisperer/codewhisperer_stack.py
@@ -5,8 +5,8 @@ from aws_cdk import (
     aws_sqs as sqs,
     aws_s3 as s3,
     aws_lambda as _lambda,
-    aws_apigateway as apigw,
-    aws_sns as sns
+    aws_sns as sns,
+    aws_apigateway as apigw
 )
 from constructs import Construct
 
@@ -29,17 +29,44 @@ class CodewhispererStack(Stack):
         
         # Create sns topic with name codewhisperer-new-example-topic
         sns_topic = sns.Topic(self, "codewhisperer-new-example-topic")
+
+        # Create api lambda function with name codewhisperer-api-lambda with inline code read from functions/api.py file and use lambda_handler handler, timeout of 300 seconds and memory size of 128 MB
+        api_lambda = _lambda.Function(self, "codewhisperer-api-lambda",
+                                        runtime=_lambda.Runtime.PYTHON_3_8,
+                                        handler="api.lambda_handler",
+                                        code=_lambda.Code.from_asset("functions"),
+                                        timeout=Duration.seconds(300),
+                                        memory_size=128)
+                                                  
+        # Create authorizer lambda function with name codewhisperer-authorizer-lambda with inline code read from functions/authorizer.py file and use lambda_handler handler
+        authorizer_lambda = _lambda.Function(self, "codewhisperer-authorizer-lambda",
+                                        runtime=_lambda.Runtime.PYTHON_3_8,
+                                        handler="authorizer.lambda_handler",
+                                        code=_lambda.Code.from_asset("functions"))
         
-        # Create api gateway resource with name codewhisperer with path /get-examples and method GET backed by lambda function get_examples, code from s3 bucket codewhisperer-examples with key lambda/get_examples.zip, runtime python 3.8, handler get_examples.lambda_handler, proxy false, rest api name codewhisperer, description codewhisperer API, deploy options stage name dev
-        apigateway = apigw.LambdaRestApi(self, "codewhisperer",
-                                         handler=_lambda.Function(self, "get_examples",
-                                                                   function_name="get_examples",
-                                                                   code=_lambda.Code.from_bucket(bucket, key="lambda/get_examples.zip"),
-                                                                   runtime=_lambda.Runtime.PYTHON_3_8,
-                                                                   handler="get_examples.lambda_handler",
-                                                                   environment={"S3_BUCKET": bucket.bucket_name},
-                                                                   tracing=_lambda.Tracing.ACTIVE),
-                                                                   proxy=False,
-                                                                   rest_api_name="codewhisperer",
-                                                                   description="codewhisperer API",
-                                                                   deploy_options=apigw.StageOptions(stage_name="dev"))
+         # Create notificator lambda function with name codewhisperer-notificator-lambda with inline code read from functions/notificator.py file and use lambda_handler handler
+        notificator_lambda = _lambda.Function(self, "codewhisperer-notificator-lambda",
+                                        runtime=_lambda.Runtime.PYTHON_3_8,
+                                        handler="notificator.lambda_handler",
+                                        code=_lambda.Code.from_asset("functions"))
+        
+        # Create Lambda rest api with name codewhisperer-api-gateway
+        api_gateway = apigw.LambdaRestApi(self, "codewhisperer-api-gateway",
+                                          handler=api_lambda,
+                                          proxy=False,
+                                          deploy_options={
+                                                'logging_level': apigw.MethodLoggingLevel.INFO,
+                                                'data_trace_enabled': True,
+                                                'metrics_enabled': True,
+                                                'tracing_enabled': True
+                                                })
+        
+        # Create get-examples resource to api_gateway root
+        examples_resource = api_gateway.root.add_resource("get-examples")
+
+        # Add GET method examples resource with get-examples route and api_lambda
+        examples_resource.add_method("GET", api_lambda)
+
+        # Create put-example resource to api_gateway root with authorizer_lambda as authorizer
+        put_example_resource = api_gateway.root.add_resource("put-example")
+        put_example_resource.add_method("PUT", api_lambda, authorizer=apigw.LambdaAuthorizer(self, "authorizer", handler=authorizer_lambda))
