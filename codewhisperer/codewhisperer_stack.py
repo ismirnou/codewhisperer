ebd7cfa01038fe2753da3448783ee7d444477697
Igor_Smirnou <Igor.Smirnou@coxautoinc.com>
2023-06-28 14:25:58 +0300

    Add api gateway

--- codewhisperer/codewhisperer_stack.py
+++ codewhisperer/codewhisperer_stack.py
@@ -2,11 +2,12 @@ from aws_cdk import (
     Duration,
     RemovalPolicy,
     Stack,
-    aws_sqs as sqs,
     aws_s3 as s3,
     aws_lambda as _lambda,
-    aws_sns as sns,
-    aws_apigateway as apigw
+    aws_apigatewayv2_alpha as apigwv2,
+    aws_apigatewayv2_integrations_alpha as apigwv2_integrations,
+    aws_apigatewayv2_authorizers_alpha as apigwv2_authorizers,
+    aws_iam as iam,
 )
 from constructs import Construct
 
@@ -21,52 +22,71 @@ class CodewhispererStack(Stack):
                            removal_policy=RemovalPolicy.DESTROY,
                            auto_delete_objects=True)
         
-        # Create sqs queue with name codewhisperer-new-example with visibility timeout of 300 seconds, wait time of 20 seconds and removal policy destroy
-        sqs_queue = sqs.Queue(self, "codewhisperer-new-example",
-                              visibility_timeout=Duration.seconds(300),
-                              receive_message_wait_time=Duration.seconds(20),
-                              removal_policy=RemovalPolicy.DESTROY)
-        
-        # Create sns topic with name codewhisperer-new-example-topic
-        sns_topic = sns.Topic(self, "codewhisperer-new-example-topic")
+        # Create a lambda function role with s3 read and write permissions for codewhisperers-examples bucket. Use inline policy. Don't use managed policy
+        role = iam.Role(self, "codewhisperer-lambda-role",
+                        assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
+                        inline_policies={
+                            "s3-policy": iam.PolicyDocument(
+                                    statements=[
+                                        iam.PolicyStatement(
+                                            actions=["s3:*"],
+                                            resources=[bucket.bucket_arn, bucket.bucket_arn + "/*"],
+                                            effect=iam.Effect.ALLOW
+                                            )
+                                            ]
+                                            )
+                                            })
 
-        # Create api lambda function with name codewhisperer-api-lambda with inline code read from functions/api.py file and use lambda_handler handler, timeout of 300 seconds and memory size of 128 MB
-        api_lambda = _lambda.Function(self, "codewhisperer-api-lambda",
+
+        # Create get-api lambda function with name codewhisperer-api-get-lambda with inline code read from functions/api-get.py file and use lambda_handler handler, timeout of 300 seconds and memory size of 128 MB and use codewhisperer-lambda-role as lambda function role
+        api_get_lambda = _lambda.Function(self, "codewhisperer-api-get-lambda",
                                         runtime=_lambda.Runtime.PYTHON_3_8,
-                                        handler="api.lambda_handler",
+                                        handler="api-get.lambda_handler",
                                         code=_lambda.Code.from_asset("functions"),
                                         timeout=Duration.seconds(300),
-                                        memory_size=128)
+                                        memory_size=128,
+                                        role=role)
+        
+        # Create put-api lambda function with name codewhisperer-api-put-lambda with inline code read from functions/api-put.py file and use lambda_handler handler, timeout of 300 seconds and memory size of 128 MB and use codewhisperer-lambda-role as lambda function role
+        api_put_lambda = _lambda.Function(self, "codewhisperer-api-put-lambda",
+                                        runtime=_lambda.Runtime.PYTHON_3_8,
+                                        handler="api-put.lambda_handler",
+                                        code=_lambda.Code.from_asset("functions"),
+                                        timeout=Duration.seconds(300),
+                                        memory_size=128,
+                                        role=role)
+
                                                   
-        # Create authorizer lambda function with name codewhisperer-authorizer-lambda with inline code read from functions/authorizer.py file and use lambda_handler handler
+        # Create authorizer lambda function with name codewhisperer-authorizer-lambda with inline code read from functions/authorizer.py file and use lambda_handler handler. Use codewhisperer-lambda-role as lambda function role
         authorizer_lambda = _lambda.Function(self, "codewhisperer-authorizer-lambda",
                                         runtime=_lambda.Runtime.PYTHON_3_8,
                                         handler="authorizer.lambda_handler",
-                                        code=_lambda.Code.from_asset("functions"))
+                                        code=_lambda.Code.from_asset("functions"),
+                                        role=role)
         
-         # Create notificator lambda function with name codewhisperer-notificator-lambda with inline code read from functions/notificator.py file and use lambda_handler handler
-        notificator_lambda = _lambda.Function(self, "codewhisperer-notificator-lambda",
-                                        runtime=_lambda.Runtime.PYTHON_3_8,
-                                        handler="notificator.lambda_handler",
-                                        code=_lambda.Code.from_asset("functions"))
-        
-        # Create Lambda rest api with name codewhisperer-api-gateway
-        api_gateway = apigw.LambdaRestApi(self, "codewhisperer-api-gateway",
-                                          handler=api_lambda,
-                                          proxy=False,
-                                          deploy_options={
-                                                'logging_level': apigw.MethodLoggingLevel.INFO,
-                                                'data_trace_enabled': True,
-                                                'metrics_enabled': True,
-                                                'tracing_enabled': True
-                                                })
-        
-        # Create get-examples resource to api_gateway root
-        examples_resource = api_gateway.root.add_resource("get-examples")
+        authorizer = apigwv2_authorizers.HttpLambdaAuthorizer("BooksAuthorizer", authorizer_lambda)
 
-        # Add GET method examples resource with get-examples route and api_lambda
-        examples_resource.add_method("GET", api_lambda)
 
-        # Create put-example resource to api_gateway root with authorizer_lambda as authorizer
-        put_example_resource = api_gateway.root.add_resource("put-example")
-        put_example_resource.add_method("PUT", api_lambda, authorizer=apigw.LambdaAuthorizer(self, "authorizer", handler=authorizer_lambda))
+        # Create api gateway with name codewhisperer-api-gateway and use codewhisperer-api-get-lambda as api get lambda function, codewhisperer-api-put-lambda as api put lambda function and use authorizer lambda function as authorizer
+        api_gateway = apigwv2.HttpApi(self, "codewhisperer-api-gateway",
+                                      api_name="codewhisperer-api-gateway",
+                                      create_default_stage=True,
+                                      default_authorizer=authorizer)
+        
+
+
+        # Add get method to api gateway with name GET and use codewhisperer-api-get-lambda as integration api_gateway_v2_integrations HttpLambdaIntegration providing id: string, function: Function. Add authorizer.
+        api_gateway.add_routes(
+            path="/get",
+            methods=[apigwv2.HttpMethod.GET],
+            integration=apigwv2_integrations.HttpLambdaIntegration('get', api_get_lambda),
+            authorizer=authorizer
+            )
+
+        # Add put method to api gateway with name PUT and use codewhisperer-api-put-lambda as integration. Use codewhisperer-authorizer-lambda as authorizer
+        api_gateway.add_routes(
+            path="/put",
+            methods=[apigwv2.HttpMethod.PUT],
+            integration=apigwv2_integrations.HttpLambdaIntegration('put', api_put_lambda),
+            authorizer=authorizer
+            )
